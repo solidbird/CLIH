3bdc57d0022455cc69ef9b7811f8b922859c6286
Serdar Akcay <serdar.akcay@alumni.fh-aachen.de>
2025-04-20 16:36:30 +0200

    CLI.h: change main datasructure to 'trident' like list

--- CLI.h
+++ CLI.h
@@ -24,7 +24,9 @@ Options:
 #ifndef CLI_H
 #define CLI_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef enum cli_type {
 	BOOL,
@@ -66,19 +68,33 @@ typedef struct cli_output {
 	cli_option_item *items;
 } cli_output;
 
+typedef struct cli_list_opt {
+	cli_option_item item;
+	struct cli_list_opt *next;
+} cli_list_opt;
+
+typedef struct cli_list_arg {
+	cli_argument_item item;
+	struct cli_list_arg *next;
+} cli_list_arg;
+
+typedef struct cli_list_cmd {
+	cli_option_item item;
+	struct cli_list_cmd *next;
+} cli_list_cmd;
+
 typedef struct cli_list {
-	cli_variant cli_var;
-	cli_option_item option_item;
-	cli_argument_item argument_item;
-	cli_command_item command_item;
-	struct cli_list *next;
+	char prog_description[50];
+	cli_list_opt *opt_head;
+	cli_list_arg *arg_head;
+	cli_list_cmd *cmd_head;
 } cli_list;
 
-int cli_init(cli_list **cli_list_obj, char *program_desc);
+int cli_init(cli_list *cli_list_obj, const char *program_desc);
 int cli_display_help(char *help_option_small, char *help_option_big);
-int cli_add_option(cli_list **cli_list_obj, char *option_flag_small, char *option_flag_big, char *descr, cli_type type);
+int cli_add_option(cli_list *cli_list_obj, char *option_flag_small, char *option_flag_big, char *descr, cli_type type);
 int cli_add_argument(char *argument_name, cli_type type);
-int cli_execute(cli_list **cli_list_obj, int argc, char **argv);
+int cli_execute(cli_list *cli_list_obj, int argc, char **argv);
 
 /*cli_init(program_descr);
 cli_add_option("-r", "--reload", "This is a description of the whole thing.", cli_type.BOOL);
@@ -89,60 +105,59 @@ cli_execute(int argc, char **argv);*/
 
 #ifdef CLI_IMPLEMENTATION
 
-int cli_init(cli_list **cli_list_obj, char *program_desc){
-	(*cli_list_obj) = malloc(sizeof(cli_list));
-	(*cli_list_obj)->cli_var = MAIN;
-	strncpy(&(*cli_list_obj)->command_item.description, program_desc, 50);
-	(*cli_list_obj)->next = NULL;
-
+int cli_init(cli_list *cli_list_obj, const char *program_desc){
+	strncpy(cli_list_obj->prog_description, program_desc, 50);
+	cli_list_obj->opt_head = NULL;
+	cli_list_obj->arg_head = NULL;
+	cli_list_obj->cmd_head = NULL;
 	return 0;
 }
 
 int cli_display_help(char *help_option_small, char *help_option_big){}
-int cli_add_option(cli_list **cli_list_obj, char *option_flag_small, char *option_flag_big, char *descr,  cli_type type){
-	cli_list *tmp_list = (*cli_list_obj);
+int cli_add_option(cli_list *cli_list_obj, char *option_flag_small, char *option_flag_big, char *descr,  cli_type type){
+	cli_list_opt **tmp_list = &cli_list_obj->opt_head;
 
-	while(tmp_list->next != NULL){
-		tmp_list = tmp_list->next;
+	if((*tmp_list) != NULL){
+		while((*tmp_list)->next != NULL){
+			(tmp_list) = &(*tmp_list)->next;
+		}
+
+		(*tmp_list)->next = malloc(sizeof(cli_list_opt));
+		tmp_list = &(*tmp_list)->next;
+
+	}else{
+	
+		(*tmp_list) = malloc(sizeof(cli_list_opt));
 	}
 
-	tmp_list->next = malloc(sizeof(cli_list));
-	tmp_list->next->cli_var = OPT;
 	if(option_flag_small){
-		strncpy(&tmp_list->next->option_item.name_small, option_flag_small, 50);
+		strncpy((*tmp_list)->item.name_small, option_flag_small, 50);
 	}
 	if(option_flag_big){
-		strncpy(&tmp_list->next->option_item.name_big, option_flag_big, 50);
+		strncpy((*tmp_list)->item.name_big, option_flag_big, 50);
 	}
-	strncpy(&tmp_list->next->option_item.description, descr, 50);
-	tmp_list->next->option_item.default_value = 0;
-	tmp_list->next->next = NULL;
+	strncpy((*tmp_list)->item.description, descr, 50);
+	(*tmp_list)->item.default_value = 0;
 
 	return 0;
-}
-int cli_add_argument(char *argument_name, cli_type type){}
-int cli_execute(cli_list **cli_list_obj, int argc, char **argv){
-	cli_list *tmp_list = (*cli_list_obj);
-	int i = 0;
 
-	while(tmp_list != NULL){
-		switch(tmp_list->cli_var){
-			case MAIN:
-				printf("Usage: %s\n\t%s\n\n", argv[0], tmp_list->command_item.description);
-			break;
-			case OPT:
-				printf("'%s, %s':\n\t\t\t%s\n",
-					tmp_list->option_item.name_small ? tmp_list->option_item.name_small : "",
-					tmp_list->option_item.name_big ? tmp_list->option_item.name_big: "",
-					tmp_list->option_item.description);
-			break;
-			case ARG:
-			break;
-			case COM:
-			break;
-		}
-		i++;
-		tmp_list = tmp_list->next;
+}
+
+int cli_add_argument(char *argument_name, cli_type type){}
+int cli_execute(cli_list *cli_list_obj, int argc, char **argv){
+	cli_list *tmp_list = cli_list_obj;
+	cli_list_opt *tmp_opt = tmp_list->opt_head;
+	cli_list_arg *tmp_arg = tmp_list->arg_head;
+	cli_list_cmd *tmp_cmd = tmp_list->cmd_head;
+
+	printf("Usage: %s\n\t%s\n\n", argv[0], tmp_list->prog_description);
+
+	while(tmp_opt != NULL){
+		printf("'%s, %s':\n\t\t\t%s\n",
+			tmp_opt->item.name_small ? tmp_opt->item.name_small : "",
+			tmp_opt->item.name_big ? tmp_opt->item.name_big: "",
+			tmp_opt->item.description);
+		tmp_opt = tmp_opt->next;
 	}
 
 	return 0;
